c540bfea81d852ad579221655d8fa87289a6ee08
danioviedo <danioviedob@gmail.com>
2022-02-28 17:10:29 -0500

    02 js listo

---
+++ 02-JS-I/homework/instruccionesreply.js
@@ -0,0 +1,5 @@
+vvariables: es un espacio donde guardamos cosas
+strings: es una cadena de texto que siempre va dentro de comillas 
+funciones(argumentos, return): argumento es lo que recibe la funcion como dato adicional; return: es para devover el valor
+declaraciones: if:permite validar algo 
+Valores booleanos: dan un resultado verdadero o false a partir de que la respuesta se cumpla o no
